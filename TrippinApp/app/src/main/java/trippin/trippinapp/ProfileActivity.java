58f542596962fa2de714d473142ac3a094173dd2
tacco <tacco@simplee.com>
2017-05-06 04:27:42 +0300

    changes

--- TrippinApp/app/src/main/java/trippin/trippinapp/ProfileActivity.java
+++ TrippinApp/app/src/main/java/trippin/trippinapp/ProfileActivity.java
@@ -3,11 +3,20 @@ package trippin.trippinapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.Date;
+
+import trippin.trippinapp.model.Trip;
+import trippin.trippinapp.model.User;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    User user = new User("atoma@gmail.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Trip t1 = new Trip(null, "Bla", new Date(), new Date("30/10.2017"))
     }
 }
