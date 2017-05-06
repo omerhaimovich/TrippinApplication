c4e74c6ceb312aa9a33c2a63b76d70e91b39310f
Hila <lilosha26@gmail.com>
2017-05-06 04:04:36 +0300

    changing names and paths

--- TrippinApp/app/src/main/java/trippin/trippinapp/model/Attraction.java
+++ TrippinApp/app/src/main/java/trippin/trippinapp/model/Attraction.java
@@ -11,28 +11,16 @@ public class Attraction {
     String m_id;
     String m_googleID;
     String m_name;
-    Date m_startTime;
-    Date m_tripEndTime;
     int m_rate;
-    Date m_createdAt;
-    Date m_updaedAt;
-    Trip m_Trip;
 
-    public Attraction(String googleID, String name, Date from_time,
-                      Date to_time, int rate, Trip trip) {
-        this(UUID.randomUUID().toString(), googleID, name, from_time, to_time, rate, trip);
+    public Attraction(String googleID, String name, int rate) {
+        this(UUID.randomUUID().toString(), googleID, name, rate);
     }
 
-    public Attraction(String _id, String m_googleID, String name,
-                      Date from_time, Date to_time, int rate, Trip trip) {
+    public Attraction(String _id, String m_googleID, String name, int rate) {
         this.m_id = _id;
         this.m_googleID = m_googleID;
         this.m_name = name;
-        this.m_startTime = from_time;
-        this.m_tripEndTime = to_time;
         this.m_rate = rate;
-        this.m_createdAt = new Date();
-        this.m_updaedAt = new Date();
-        this.m_Trip = trip;
     }
 }
