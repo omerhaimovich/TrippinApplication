e04fb4a7b13a8cf172730ccdf1d92a1df80a8195
hila <lilosha26@gmail.com>
2017-05-08 19:22:33 +0300

    adding demo attraction to map

--- TrippinApp/app/src/main/java/trippin/trippinapp/activities/MapsActivity.java
+++ TrippinApp/app/src/main/java/trippin/trippinapp/activities/MapsActivity.java
@@ -51,27 +51,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        ArrayList<Attraction> attractions = new ArrayList<>();
+        // TODO: 06-May-17 Hila - check this with real attractions from server API
+        String attractionName1 = ("Charing Cross Underground Station");
+        String attractionName2 = ("Sherlock Holmes");
 
-        // TODO: 06-May-17 Hila - check this with real attractions from server API 
-        for (int i = 0; i < 10; i++) {
-            String attractionName = ("Attraction" + i);
-            LatLng attractionPos = new LatLng((-34 - i * 2), (151 + i * 5));
+        LatLng attractionPos1 = new LatLng(51.507852, -0.127252);
+        LatLng attractionPos2 = new LatLng(51.507345, -0.12534);
 
-            Attraction attraction = new Attraction("gdsfg",
-                    attractionName,
-                    1,
-                    attractionPos, null, null);
-            attractions.add(attraction);
+        Attraction attraction1 = new Attraction("gdsfg",
+                attractionName1, 1, attractionPos1, null, null);
 
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.title(attractionName);
-            markerOptions.position(attractionPos);
-            markerOptions.icon(attraction.getImage());
-            mMap.addMarker(markerOptions);
-        }
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.title(attractionName1);
+        markerOptions1.position(attractionPos1);
+        markerOptions1.icon(attraction1.getImage());
+        mMap.addMarker(markerOptions1);
+
+        Attraction attraction2 = new Attraction("gdsfg",
+                attractionName2, 1, attractionPos2, null, null);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.title(attractionName2);
+        markerOptions2.position(attractionPos2);
+        markerOptions2.icon(attraction2.getImage());
+        mMap.addMarker(markerOptions2);
 
         // Add a marker in Sydney and move the camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(attractions.get(0).getAttractionLocation()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(attractionPos1));
+    }
+
+    public void btnStartTrippin(View view) {
+        String attractionName3 = ("Garfunkel's");
+        String attractionName4 = ("MyMindWorks Ltd");
+        String attractionName5 = ("Pizza Express");
+
+
+        LatLng attractionPos3 = new LatLng(51.507196, -0.126829);
+        LatLng attractionPos4 = new LatLng(51.508126, -0.127132);
+        LatLng attractionPos5 = new LatLng(51.508448, -0.126147);
+
+        Attraction attraction3 = new Attraction("gdsfg",
+                attractionName3, 1, attractionPos3, null, null);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.title(attractionName3);
+        markerOptions3.position(attractionPos3);
+        markerOptions3.icon(attraction3.getImage());
+        mMap.addMarker(markerOptions3);
+
+        Attraction attraction4 = new Attraction("gdsfg",
+                attractionName4, 1, attractionPos4, null, null);
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.title(attractionName4);
+        markerOptions4.position(attractionPos4);
+        markerOptions4.icon(attraction4.getImage());
+        mMap.addMarker(markerOptions4);
+
+
+        Attraction attraction5 = new Attraction("gdsfg",
+                attractionName5, 1, attractionPos5, null, null);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.title(attractionName5);
+        markerOptions5.position(attractionPos5);
+        markerOptions5.icon(attraction5.getImage());
+        mMap.addMarker(markerOptions5);
+
     }
 }
