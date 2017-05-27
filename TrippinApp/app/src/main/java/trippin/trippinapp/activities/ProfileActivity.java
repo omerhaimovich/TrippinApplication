10fc0cd5d2b6379b0f5722db5f2e2535029dc615
bareliah <bareliah@gmail.com>
2017-05-27 14:13:39 +0300

    some changes

--- TrippinApp/app/src/main/java/trippin/trippinapp/activities/ProfileActivity.java
+++ TrippinApp/app/src/main/java/trippin/trippinapp/activities/ProfileActivity.java
@@ -1,18 +1,27 @@
 package trippin.trippinapp.activities;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.os.StrictMode;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
+import java.io.IOException;
+import java.io.IOException;
 import java.util.Date;
 
 import trippin.trippinapp.R;
@@ -20,14 +29,19 @@ import trippin.trippinapp.adapter.TripsAdapter;
 import trippin.trippinapp.model.Attraction;
 import trippin.trippinapp.model.Trip;
 import trippin.trippinapp.model.User;
+import trippin.trippinapp.serverAPI.RequestHandler;
+import trippin.trippinapp.R;
+import trippin.trippinapp.serverAPI.RequestHandler;
 
 public class ProfileActivity extends ListActivity {
 
     public void EndTrip(View view) {
         ((Button)findViewById(R.id.btnEndTripProfile)).setVisibility(View.INVISIBLE);
         ((Button)findViewById(R.id.btnStartTripProfile)).setVisibility(View.VISIBLE);
-        //TODO : Bar need to continue
-        //RequestHandler.endTrip();
+
+        if(User.getCurrentUser().getCurrentTrip() == Trip){
+            RequestHandler.
+        }
     }
 
     public void StartTrip(View view) {
@@ -40,17 +54,16 @@ public class ProfileActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Trip t1 = new Trip(null, "Berlin", new Date(), null);
-        t1.getAttractions().add(new Attraction(null, "Star Bucks", 5, null, new Date(), null));
-        t1.getAttractions().add(new Attraction(null, "Zara", 4, null, new Date(),new Date()));
-        t1.getAttractions().add(new Attraction(null, "Mcdonalds", 4, null, new Date(),new Date()));
-        Trip t2 = new Trip(null, "London", new Date("20/12/2015"), new Date("29/12/2015"));
-        Trip t3 = new Trip(null, "Paris", new Date("05/08/2016"), new Date("13/08/2016"));
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            User.getCurrentUser().UpdateTrips(RequestHandler.connectUser(User.getCurrentUser().getEmail(), (double)0, (double)0).getAsJsonObject());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         User user = User.getCurrentUser();
-        user.addNewTrip(t1);
-        user.addNewTrip(t2);
-        user.addNewTrip(t3);
-        user.setCurrentTrip(t1);
 
         final ImageView image = ((ImageView)findViewById(R.id.profile_image));
 
